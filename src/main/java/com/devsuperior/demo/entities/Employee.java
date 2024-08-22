28553e4abadca2ee486c91008c815dce014ff6d6
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-08-22 14:36:48 -0300

    implemented tests

--- src/main/java/com/devsuperior/demo/entities/Employee.java
+++ src/main/java/com/devsuperior/demo/entities/Employee.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tb_employee")
